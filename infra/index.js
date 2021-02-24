bafdd0990d37622176fd856614fe346610dab7bf
Yurii Datsenko <pinapbuss@gmail.com>
2021-02-24 23:44:17 +0200

    Added cloudFront instruction to index.js

--- infra/index.js
+++ infra/index.js
@@ -31,5 +31,48 @@ const bucketPolicy = new aws.s3.BucketPolicy("bucketPolicy", {
 
 exports.bucketName = bucket.id;
 
+const tenMinutes = 60 * 10;
 
+const s3OriginId = "myS3Origin";
+const cdn = new aws.cloudfront.Distribution("s3Distribution", {
+    origins: [{
+        domainName: bucket.bucketRegionalDomainName,
+        originId: s3OriginId,
+    }],
+    enabled: true,
+    defaultRootObject: "index.html",
+
+    defaultCacheBehavior: {
+        allowedMethods: [
+            "GET",
+            "HEAD",
+	     "POST",
+        ],
+        cachedMethods: [
+            "GET",
+            "HEAD",
+            "POST",
+        ],
+        targetOriginId: s3OriginId,
+        forwardedValues: {
+            queryString: false,
+            cookies: {
+                forward: "none",
+            },
+        },
+        viewerProtocolPolicy: "https-only",
+        minTtl: 0,
+        defaultTtl: tenMinutes,
+        maxTtl: tenMinutes,
+    },
+    restrictions: {
+        geoRestriction: {
+            restrictionType: "none",
+        },
+    },
+    priceClass: "PriceClass_100",
+    viewerCertificate: {
+        cloudfrontDefaultCertificate: true,
+    },
+});
 
